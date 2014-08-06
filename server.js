2fb1c0e986c66348ff82e77fde4ca903aef08ab3
Nathan White <nw@nwhite.net>
2014-08-06 14:54:44 -0600

    implemented server rendering, wrapped in build step.. not the prettiest

--- server.js
+++ server.js
@@ -5,55 +5,59 @@ var express = require('express')
   , app = express()
   , component = require('./lib/react-component')
   , compile_config = require('./webpack.config')
-  , compiler = webpack(compile_config);
+  , template_config = require('./webpack.templates')
+  , compiler = webpack(compile_config)
+  , tpl_compiler = webpack(template_config);
 
-// register components
-// require('./client/components/comments');
-
-var comments = [{author: 'Pete Hunt', text: 'Hey there!'}];
-
-app
-  .set('views', __dirname + '/public')
-  .set('view options', {layout: false})
-  .set('view engine', 'jade');
-
-app.use(webpackDevMiddleware(compiler, {
-    quiet: false,
-    noInfo: false,
-    lazy: false,
-    watchDelay: 100,
-    publicPath: "/js/",
-    stats: { colors: true }
-}));
-
-app.get('/', function(req, res){
+tpl_compiler.run(function(){
   
-  // var component.renderString('CommentBox', {
-  //     data: comments
-  //   , url: "comments.json"
-  //   , pollInterval: 2000})
-  // });
-  
-  res.render("index", {
-    content: ""
-  })
+  // register components
+  require('./client/components/comments');
+
+  var comments = [{author: 'Pete Hunt', text: 'Hey there!'}];
+
+  app
+    .set('views', __dirname + '/public')
+    .set('view options', {layout: false})
+    .set('view engine', 'jade');
+
+  app.use(webpackDevMiddleware(compiler, {
+      quiet: false,
+      noInfo: false,
+      lazy: false,
+      watchDelay: 100,
+      publicPath: "/js/",
+      stats: { colors: true }
+  }));
+
+
+  app.get('/', function(req, res){
+    res.render("index", {
+      content: component.renderString('CommentBox', {
+          data: comments
+        , url: "comments.json"
+        , pollInterval: 2000
+      })
+    });
+  });
+
+  app.use('/', express.static(__dirname + '/public'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  app.get('/comments.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(comments));
+  });
+
+  app.post('/comments.json', function(req, res) {
+    comments.push(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(comments));
+  });
+
+  app.listen(3000);
+
+  console.log('Server started: http://localhost:3000/');
+
 });
-
-app.use('/', express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/comments.json', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
-});
-
-app.post('/comments.json', function(req, res) {
-  comments.push(req.body);
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
-});
-
-app.listen(3000);
-
-console.log('Server started: http://localhost:3000/');
